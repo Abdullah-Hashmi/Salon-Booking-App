0b3e6887beeb75b512ea414506b13262f06fdacb
Syed Abdullah <syedabdullah.developer@gmail.com>
2024-10-07 09:30:16 +0500

    Update GlobalStyles.tsx

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,13 +1,13 @@
 /* fonts */
 export const FontFamily = {
-  captionsCaption2L: "Montserrat-Bold",
-  captionsCaption3M: "Montserrat-SemiBold",
-  bodyBody4: "Montserrat-Regular",
-  montserratBlack: "Montserrat-Black",
-  buttonsB2M: "Poppins-Medium",
-  headingGazpachoH7: "Poppins-Bold",
-  tagsT1SB: "Poppins-SemiBold",
-  paragraphP2: "Poppins-Regular",
+  captionsCaption2L: 'Montserrat-Bold',
+  captionsCaption3M: 'Montserrat-SemiBold',
+  bodyBody4: 'Montserrat-Regular',
+  montserratBlack: 'Montserrat-Black',
+  buttonsB2M: 'Poppins-Medium',
+  headingGazpachoH7: 'Poppins-Bold',
+  tagsT1SB: 'Poppins-SemiBold',
+  paragraphP2: 'Poppins-Regular',
 };
 /* font sizes */
 export const FontSize = {
@@ -20,31 +20,86 @@ export const FontSize = {
 };
 /* Colors */
 export const Color = {
-  neutral050: "#fff",
-  elevation2: "#21222a",
-  elevation0: "#181a1f",
-  opacity5b: "rgba(0, 0, 0, 0.05)",
-  opacity60b: "rgba(0, 0, 0, 0.6)",
-  opacity10b: "rgba(0, 0, 0, 0.1)",
-  opacity35b: "rgba(0, 0, 0, 0.35)",
-  colorPlum_100: "rgba(205, 190, 255, 0.08)",
-  neutral400: "#9a96a1",
-  primary600: "#6429fa",
-  colorDarkslateblue: "#865c98",
-  primary300: "#b29dff",
-  error600: "#d5182a",
-  neutral800: "#312f37",
-  neutral600: "#605d66",
-  neutral200: "#e6e1e6",
-  secondary500: "#b94fc6",
-  neutral300: "#c9c4cf",
-  success400: "#2fe98c",
-  error050: "#fefafa",
-  primaryBlack: "#000",
-  businessPeriwinkle: "#c8d0e3",
-  colorLightsteelblue_100: "rgba(200, 208, 227, 0)",
-  othersOutrageousOrange: "#fc542e",
-  primarySunHaze: "#f6eddd",
+  // ****************************************
+  // Global colors for salon app
+
+  // → Brand
+  Purple_4: 'CBBFFF',
+  Purple_3: '9880FE',
+  Purple_2_Base: '6440FE',
+  Purple_1: '3F13FE',
+  Purple_0: '2C01E2',
+
+  // → Base
+  Dark_0: '1C1C28',
+  Dark_1: '28293D',
+  Dark_2: '555770',
+  Dark_3: '8F90A6',
+  Dark_4: 'C7C9D9',
+
+  Light_0: 'E4E4EB',
+  Light_1: 'EBEBF0',
+  Light_2: 'F2F2F5',
+  Light_3: 'FAFAFC',
+  Light_4: 'FFFFFF',
+
+  // → System
+  Orange_0: 'E57A00',
+  Orange_1: 'FF8800',
+  Orange_2: 'FDAC42',
+  Orange_3: 'FCCC75',
+  Orange_4: 'FFF8E5',
+
+  Blue_0: '004FC4',
+  Blue_1: '0063F7',
+  Blue_2: '5B8DEF',
+  Blue_3: '9DBFF9',
+  Blue_4: 'E5F0FF',
+
+  Yellow_0: 'E5B800',
+  Yellow_1: 'FFCC00',
+  Yellow_2: 'FDDD48',
+  Yellow_3: 'FDED72',
+  Yellow_4: 'FFFEE5',
+
+  Green_0: '05A660',
+  Green_1: '06C270',
+  Green_2: '39D98A',
+  Green_3: '57EBA1',
+  Green_4: 'E3FFF1',
+
+  Red_0: 'E53535',
+  Red_1: 'FF3B3B',
+  Red_2: 'FF5C5C',
+  Red_3: 'FF8080',
+  Red_4: 'FFE5E5',
+
+  // **************************************
+  neutral050: '#fff',
+  elevation2: '#21222a',
+  elevation0: '#181a1f',
+  opacity5b: 'rgba(0, 0, 0, 0.05)',
+  opacity60b: 'rgba(0, 0, 0, 0.6)',
+  opacity10b: 'rgba(0, 0, 0, 0.1)',
+  opacity35b: 'rgba(0, 0, 0, 0.35)',
+  colorPlum_100: 'rgba(205, 190, 255, 0.08)',
+  neutral400: '#9a96a1',
+  primary600: '#6429fa',
+  colorDarkslateblue: '#865c98',
+  primary300: '#b29dff',
+  error600: '#d5182a',
+  neutral800: '#312f37',
+  neutral600: '#605d66',
+  neutral200: '#e6e1e6',
+  secondary500: '#b94fc6',
+  neutral300: '#c9c4cf',
+  success400: '#2fe98c',
+  error050: '#fefafa',
+  primaryBlack: '#000',
+  businessPeriwinkle: '#c8d0e3',
+  colorLightsteelblue_100: 'rgba(200, 208, 227, 0)',
+  othersOutrageousOrange: '#fc542e',
+  primarySunHaze: '#f6eddd',
 };
 /* Style Variables */
 export const StyleVariable = {
