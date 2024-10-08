818565c01ed4e834307d887c38e105d857122502
Syed Abdullah <syedabdullah.developer@gmail.com>
2024-10-08 19:06:43 +0500

    updated assets

---
+++ src/constants/images.tsx
@@ -0,0 +1,11 @@
+const Intro_1_BG = require('../assets/images/Intro1-BG-Image.png');
+const Intro_2_BG = require('../assets/images/Intro2-BG-Image.png');
+const Intro_3_BG = require('../assets/images/Intro3-BG-Image.png');
+const Intro_4_BG = require('../assets/images/Intro4-BG-Image.png');
+
+export default {
+  Intro_1_BG,
+  Intro_2_BG,
+  Intro_3_BG,
+  Intro_4_BG,
+};
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,1 +1,5 @@
 import appTheme from './themes';
+import icons from './icons';
+import {FONTS} from './fonts';
+
+export {appTheme, icons, FONTS};
