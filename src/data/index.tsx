0fad0cc3070dfa889aba330284899e43a53f0212
Syed Abdullah <syedabdullah.developer@gmail.com>
2024-10-18 12:47:40 +0500

    update Onboarding

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -4,25 +4,22 @@ export const slides = [
   {
     id: 1,
     image: images.Intro_1_BG,
-    title: 'Salon Booking App',
-    description: 'Book your next salon appointment',
+    title: 'Schedule your Appointment with the best Hair Stylist in your Town.',
   },
   {
     id: 2,
     image: images.Intro_2_BG,
-    title: 'Salon Booking App',
-    description: 'Book your next salon appointment',
+    title: 'Schedule the Appointmentin the best Salon for yourKids.',
   },
   {
     id: 3,
     image: images.Intro_3_BG,
-    title: 'Salon Booking App',
-    description: 'Book your next salon appointment',
+    title: 'Book yourself a massage therapist to release all your stress. ',
   },
   {
     id: 4,
     image: images.Intro_4_BG,
-    title: 'Salon Booking App',
-    description: 'Book your next salon appointment',
+    title:
+      'Search for the best parlournear you to fulfil all your beauty needs',
   },
 ];
--- src/screens/Onboarding.jsx
+++ src/screens/Onboarding.jsx
@@ -7,6 +7,7 @@ import {
   StatusBar,
   FlatList,
   Image,
+  TouchableOpacity,
 } from 'react-native';
 import {slides} from '../data';
 import React from 'react';
@@ -16,21 +17,49 @@ const {height, width} = Dimensions.get('window');
 
 const Slide = ({item}) => {
   return (
-    <View style={{alignItems: 'center'}}>
+    <View style={{width, height}}>
       <Image
         source={item?.image}
-        style={{width: width, height: '75%', resizeMode: 'contain'}}
+        style={{height: '100%', width: '100%', resizeMode: 'cover'}}
       />
-
-      <View>
+      {/* text overlay */}
+      <View style={styles.textContainer}>
         <Text style={styles.title}>{item?.title}</Text>
-        <Text style={styles.subtitle}>{item?.subtitle}</Text>
       </View>
     </View>
   );
 };
 
-const Onboarding = () => {
+const Onboarding = ({navigation}) => {
+  const [currentSlideIndex, setCurrentSlideIndex] = React.useState(0);
+  const Footer = () => {
+    return (
+      <View style={styles.footerContainer}>
+        {/* indicator container */}
+        <View style={styles.indicatorContainer}>
+          {/* render indicator */}
+          {slides.map((_, index) => (
+            <View
+              key={index}
+              style={[
+                styles.indicator,
+                index === currentSlideIndex && {
+                  backgroundColor: Colors.Light_4,
+                },
+              ]}></View>
+          ))}
+        </View>
+        <View style={{marginBottom: 20}}>
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity>
+              <Text>Next</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView
       style={{
@@ -40,15 +69,55 @@ const Onboarding = () => {
       <StatusBar backgroundColor={Colors.white} />
       <FlatList
         data={slides}
-        contentContainerStyle={{height: height * 0.75}}
         renderItem={({item}) => <Slide item={item} />}
         horizontal
+        pagingEnabled
         showsHorizontalScrollIndicator={false}
       />
+      {/* Render footer component here */}
+      <Footer />
     </SafeAreaView>
   );
 };
 
 export default Onboarding;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textContainer: {
+    position: 'absolute',
+    bottom: 100, // Position title above footer
+    width: '100%',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    color: Colors.Light_4,
+    fontSize: FontSize.H4_Light,
+    fontFamily: FontFamily.H4_Regular,
+    textShadowOffset: {width: 4, height: 1},
+    textShadowRadius: 15,
+    fontWeight: 'regular',
+  },
+  footerContainer: {
+    position: 'absolute',
+    bottom: 16, // Position footer at the bottom
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+  indicatorContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20,
+    // color: Colors.Light_0,
+  },
+  indicator: {
+    height: 10,
+    width: 10, // Adjust the size of the indicator
+    backgroundColor: Colors.Dark_2,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginHorizontal: 5,
+  },
+  buttonContainer: {},
+});
